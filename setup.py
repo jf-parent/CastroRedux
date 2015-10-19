a5b90f2324ba46ec4f76f86e845b2ef24e05eb62
hacker-pyrat <parent.j.f@gmail.com>
2015-10-19 16:42:05 -0400

    fixs

--- setup.py
+++ setup.py
@@ -11,14 +11,9 @@ except ImportError:
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = []
-with open('requirements.txt', 'r') as fd:
-    for line in fd:
-        requirements.append(line)
-
 setup(
     name='castroredux',
-    version='0.1.1',
+    version='0.1.2',
     description="'screencast robot' - a tiny fork of vnc2flv",
     long_description=readme,
     author="Brome-HQ",
@@ -32,7 +27,6 @@ setup(
     package_dir={'castroredux':
                  'castroredux'},
     include_package_data=True,
-    install_requires=requirements,
     license="ISCL",
     zip_safe=False,
     keywords='castroredux',
